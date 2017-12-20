dc03d3e42060477715c1802d6ba1554a8ca46f69
hellomrgavin <mrgavinchan@163.com>
2017-12-20 22:29:49 +0800

    提交注释

--- ID3/rpartPackageUsage.r
+++ ID3/rpartPackageUsage.r
@@ -2,8 +2,11 @@ SNS<-read.csv("./DataSource/SNS.data.csv")
 
 library(rpart)
 
-iris.rp<-rpart(class~.,data = iris,method = "class")
-plot(iris.rp,uniform = T,branch = 0,margin = 0.1,main="iris ID3")
-text(iris.rp,use.n = T,col="blue",cex=0.9)
+#使用rpart包并传参数
+iris.rp<-rpart(class~.,data = iris,method = "class") 
 
-fancyRpartPlot(iris.rp)
+#画图
+plot(iris.rp,uniform = T,branch = 0,margin = 0.1,main="iris ID3")#http://f.dataguru.cn/thread-121228-1-1.html
+text(iris.rp,use.n = T,col="blue",cex=1.2) #use.n 是控制下边50/0/0样本分类概况，col字体颜色、cex 字体大小
+
+
